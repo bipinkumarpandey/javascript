bc0b60857cc6894c194830869f0e78815cd39c93
bipinkumarpandey <bipinjsr05@gmail.com>
2024-02-06 07:01:55 +0000

    data summaries

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,36 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Javascipt is dyamnic typed language
+
+const score = 100
+
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bigNumber = 342342342342342345454n
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman","naagraj","doga"]
+
+let myobj = {
+    id:12,
+    name:"Bipin",
+    age:22
+}
