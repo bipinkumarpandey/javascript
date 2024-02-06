a3363a6678979afa212d7574fe277660d3ff7c43
bipinkumarpandey <bipinjsr05@gmail.com>
2024-02-06 06:30:30 +0000

    Operations

--- 01_Basics/03_conversionOperations.js
+++ 01_Basics/03_conversionOperations.js
@@ -1,8 +1,16 @@
 let score = "Bipin"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+
+// **************************** Operations **********************************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
